97a1a971a32c5a6d601fdcfabd1fbc50bb33b2f7
Raymond Benefield <raymond.benefield@gmail.com>
2018-09-04 10:08:37 -0700

    dx(vuepress): Add dynamic sidebar based on directory structure.

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,47 @@
+const dirTree = require('directory-tree')
+const path = require('path')
+const merge = require('lodash.mergewith')
+const titleCase = require('lodash.startcase')
+const isArray = require('lodash.isarray')
+
+const root = path.join(__dirname, '../')
+var projects = []
+dirTree(
+    root,
+    { extensions: /\.md/, exclude: /\/node_modules\// },
+    (item, PATH) => projects.push(item)
+)
+const concatArrays = (objValue, srcValue) => {
+    if (isArray(objValue)) {
+        return objValue.concat(srcValue)
+    }
+}
+projects = projects.reduce((sidebar, project) => {
+    const filePath = path.relative(root, project.path)
+    const file = path.parse(filePath)
+    return merge(
+        sidebar,
+        { [file.dir]: [`${file.dir}/${file.name}`] },
+        concatArrays
+    )
+}, {})
+
+const sidebar = Object.entries(projects).map(
+    ([group, children]) =>
+        !group
+            ? ['/', 'Home']
+            : {
+                  title: titleCase(group),
+                  collapsable: false,
+                  children: children,
+              }
+)
+
 module.exports = {
     title: 'Forge Design Document',
     description: "Design Document for Halo's Forge Engine",
+    themeConfig: {
+        displayAllHeaders: true,
+        sidebar,
+    },
 }
